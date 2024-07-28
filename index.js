5a4d09cbb394fa3a3bab3cf4e77f5a5a231d64ce
theknower <shinacrypt5@gmail.com>
2024-07-27 19:57:11 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let container = document.querySelector(".container");
+let gmail = document.querySelector(".gmail");
+let success = document.querySelector(".success");
+let btn = document.querySelector(".button");
+let inputs = document.querySelector(".input");
+let error = document.querySelector(".error");
+
+btn.addEventListener("click", function () {
+  let input = document.querySelector(".input").value;
+  if (input.includes("@")) {
+    container.classList.add("hidden");
+    success.classList.remove("hidden");
+    gmail.textContent = input;
+  } else {
+    inputs.style.color = "red";
+    error.classList.remove("hidden");
+  }
+});
